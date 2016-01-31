ffe484506eb2530518d604f332f186126eca2e6f
mydirtyhammes <mydirtyhammes@users.noreply.github.com>
2016-01-31 17:41:23 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@
   }));
   app.use(cookieParser());
 
-  app.use(express.static(path.join(__dirname, '../client')));
+  app.use(express.static(path.join(__dirname, '../Content')));
 
   app.use('/', routes);
 
